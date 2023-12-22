bbbbfe42762ce2c7839bb835f335c35ddba7afe1
aleksejbychkov <aleksej.bychkov.99@bk.ru>
2023-12-23 02:19:42 +0300

    lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 821;
+int c = 9;
+int d = 4;
+int e = 12;
+
+int max = a;
+
+if(b>max)
+{
+    max = b;
+}
+if(c>max)
+{
+    max = c;
+}
+if(d>max)
+{
+    max = d;
+}
+if(e>max)
+{
+    max = e;
+}
+Console.WriteLine(max);
