a7c5241a55af6786393f9179b4393ecacd5cf89a
aleksejbychkov <aleksej.bychkov.99@bk.ru>
2024-01-20 02:29:53 +0300

    task3_1

---
+++ LESSON_3/task3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = new int[5];
+arr[0] = 4;
+arr[1] = 7;
+arr[2] = 1;
+arr[3] = 3;
+arr[4] = 5;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+
+
+int[] arr3 = { 7, 5, 2, 6, 1 };
+
+
