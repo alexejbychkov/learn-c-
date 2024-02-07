d8d64d03b69cc145749f239daed55b419b4ef7d2
aleksejbychkov <aleksej.bychkov.99@bk.ru>
2024-02-07 13:39:26 +0300

    functions_task1

---
+++ LESSON_3/task3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] array = { 1, -4, 2, 5, -6, 2, 1, 5, -6, 8 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+        i = i + 1;
+    
+}
---
+++ LESSON_3/task3_4/Program.cs
@@ -0,0 +1,33 @@
+﻿int n = 5;
+int[] array = { 52, 23, 753, 252, 125 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+    if(array[i] > max)
+   {
+    max = array[i];
+    }
+   i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+    if(array[j] > max)
+   {
+    max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+
+foreach(int e in array)
+{
+    if(e > max)
+   {
+    max = e;
+    }
+}
+Console.WriteLine(max);
